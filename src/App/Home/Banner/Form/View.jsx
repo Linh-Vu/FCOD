549de0b7bef9db2c8c1d5c9d86ecf536d7eb0599
SAI\nguye <trinh.nguyen@ever-rise.co.jp>
2017-11-08 14:06:15 +0700

    change html of banner

--- src/App/Home/Banner/Form/View.jsx
+++ src/App/Home/Banner/Form/View.jsx
@@ -7,19 +7,58 @@ import resource from './resources';
 const FormView = () => {
   return (
     <form className="banner-right-form">
-      <div>
-        <SelectBox options={[]} />
-        <SelectBox options={[]} />
+      <div className="banner-right-form-header row">
+        <label className="col-50">Nơi Nhận</label>
+        <label className="col-50">Nơi Đi</label>
       </div>
-
-      <hr/><hr/>
-
-      <div>
-        <SelectBox options={[]} />
-        <SelectBox options={[]} />
+      <div className="banner-right-form-content row">
+        <div className="col-50">
+          <div className="content-row">
+            <label className="col-25">TP/Tỉnh :</label>
+            <select className="col-75">
+              <option value="hcm">Hồ Chí Minh</option>
+              <option value="hn">Hà Nội</option>
+              <option value="dn">Đà Nẵng</option>
+            </select>
+          </div>
+          <div className="content-row">
+            <label className="col-25">Quận/Huyện :</label>
+            <select className="col-75">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+            </select>
+          </div>
+          <div className="content-row">
+            <label className="col-25">Cân Nặng :</label>
+            <input className="col-75" type="number"></input>
+          </div>
+          <div className="content-row">
+            <label className="col-25">Giá trị :</label>
+            <input className="col-75" type="number"></input>
+          </div>
+        </div>
+        <div className="col-20"></div>
+        <div className="col-30">
+          <div className="content-row">
+            <select>
+              <option value="hcm">Hồ Chí Minh</option>
+              <option value="hn">Hà Nội</option>
+              <option value="dn">Đà Nẵng</option>
+            </select>
+          </div>
+          <div className="content-row">
+            <select>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+            </select>
+          </div>
+        </div>
       </div>
-
-      <button type="button" onClick={resource.getFee}>View</button>
+      <div className="banner-right-form-footer row">
+        <button type="submit"> send</button>
+      </div>
     </form>
   )
 };
