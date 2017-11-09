109f8f6a423255694419a49dc13dab66f54b451d
SAI\nguye <trinh.nguyen@ever-rise.co.jp>
2017-11-09 12:04:44 +0700

    change css banner-content-right

--- src/App/Home/Banner/Form/SelectBox/index.jsx
+++ src/App/Home/Banner/Form/SelectBox/index.jsx
@@ -3,15 +3,26 @@ import T from 'prop-types';
 
 import './styles.css';
 
-const SelectBox = ({ options, label }) => {
+const SelectBox = ({options, label}) => {
   return (
     <div className="select-main">
-      <label>{label ? label : 'test'}</label>
-      <select className="default">
-        {options.map((item, index) =>
-          <option key={index}>{item}</option>
-        )}
-      </select>
+      {label ?
+        <div className="row">
+          <label className="col-40">{label}</label>
+          <select className="default col-60">
+            {options.map((item, index) =>
+              <option key={index}>{item}</option>
+            )}
+          </select>
+        </div>
+        :
+        <select className="default col-100">
+          {options.map((item, index) =>
+            <option key={index}>{item}</option>
+          )}
+        </select>
+      }
+
     </div>
   )
 };
--- src/App/Home/Banner/Form/View.jsx
+++ src/App/Home/Banner/Form/View.jsx
@@ -8,7 +8,12 @@ const FormView = () => {
   return (
     <form className="banner-right-form">
       <div className="banner-right-form-header row">
-        <label className="col-50">Nơi nhận</label>
+        <div className="col-50">
+          <div className="row">
+            <div className="col-40"></div>
+            <label className="col-60">Nơi đi</label>
+          </div>
+        </div>
         <div className="col-20"></div>
         <label className="col-30">Nơi đến</label>
       </div>
@@ -16,25 +21,26 @@ const FormView = () => {
       <div className="banner-right-form-content row">
         <div className="start col-50">
           <div className="content-row">
-            <SelectBox options={[]} label={'Tỉnh/TP *:'} />
+            <SelectBox options={[]} label={'Tỉnh/TP *:'}/>
             {/*<label className="col-40">TP/Tỉnh :</label>*/}
             {/*<select className="col-60">*/}
-              {/*<option value="hcm">Hồ Chí Minh</option>*/}
-              {/*<option value="hn">Hà Nội</option>*/}
-              {/*<option value="dn">Đà Nẵng</option>*/}
+            {/*<option value="hcm">Hồ Chí Minh</option>*/}
+            {/*<option value="hn">Hà Nội</option>*/}
+            {/*<option value="dn">Đà Nẵng</option>*/}
             {/*</select>*/}
           </div>
           <div className="content-row">
-            <label className="col-40">Quận/Huyện :</label>
-            <select className="col-60">
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-            </select>
+            <SelectBox options={[]} label={'Quận/Huyện *:'}/>
+            {/*<label className="col-40">Quận/Huyện :</label>*/}
+            {/*<select className="col-60">*/}
+              {/*<option value="1">1</option>*/}
+              {/*<option value="2">2</option>*/}
+              {/*<option value="3">3</option>*/}
+            {/*</select>*/}
           </div>
           <div className="content-row">
             <label className="col-40">Cân Nặng :</label>
-            <input className="col-60" placeholder="cân nặng món hàng..."type="number"></input>
+            <input className="col-60" placeholder="cân nặng món hàng..." type="number"></input>
           </div>
           <div className="content-row">
             <label className="col-40">Giá trị :</label>
@@ -44,18 +50,20 @@ const FormView = () => {
         <div className="direction col-20"></div>
         <div className="end col-30">
           <div className="content-row">
-            <select className="col-100">
-              <option value="hcm">Hồ Chí Minh</option>
-              <option value="hn">Hà Nội</option>
-              <option value="dn">Đà Nẵng</option>
-            </select>
+            <SelectBox options={[]}/>
+            {/*<select className="col-100">*/}
+              {/*<option value="hcm">Hồ Chí Minh</option>*/}
+              {/*<option value="hn">Hà Nội</option>*/}
+              {/*<option value="dn">Đà Nẵng</option>*/}
+            {/*</select>*/}
           </div>
           <div className="content-row">
-            <select className="col-100">
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-            </select>
+            <SelectBox options={[]}/>
+            {/*<select className="col-100">*/}
+            {/*<option value="1">1</option>*/}
+            {/*<option value="2">2</option>*/}
+            {/*<option value="3">3</option>*/}
+            {/*</select>*/}
           </div>
         </div>
       </div>
