bcda1ec408386a77b1533aebd1de4ddd42be0df8
linh-vu <linh.vu@quoine.com>
2017-11-10 19:29:08 +0700

    update select box

--- src/App/Home/Banner/Form/SelectBox/index.jsx
+++ src/App/Home/Banner/Form/SelectBox/index.jsx
@@ -5,33 +5,20 @@ import './styles.css';
 
 const SelectBox = ({options, label}) => {
   return (
-    <div className="row">
-      {label ?
-        <div className="content-row">
-          <label className="col-40">{label}</label>
-          <select className="col-60">
-            {options.map((item, index) =>
-              <option key={index}>{item}</option>
-            )}
-          </select>
-        </div>
-        :
-        <div className="content-row">
-          <select className="col-100">
-            {options.map((item, index) =>
-              <option key={index}>{item}</option>
-            )}
-          </select>
-        </div>
-      }
-
+    <div className="select-main">
+      {!!label && <label>{label}</label>}
+      <select className="default">
+        {options.map((item, index) =>
+          <option key={index}>{item}</option>
+        )}
+      </select>
     </div>
   )
 };
 
 SelectBox.propTypes = {
   options: T.array.isRequired,
-  label: T.string.isRequired,
+  label: T.string,
 };
 
 export default SelectBox;
