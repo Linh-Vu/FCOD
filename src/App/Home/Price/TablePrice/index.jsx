78302b898e16ccfbd3ce7c98767e027d311afe41
Nguyen Ba Trinh <nguyenbatrinh0211@gmail.com>
2017-11-11 09:59:44 +0700

    add new element TablePrice

---
+++ src/App/Home/Price/TablePrice/index.jsx
@@ -0,0 +1,30 @@
+import T from 'prop-types';
+import React from 'react';
+
+import 'styles.css';
+
+const TablePrice = ({list}) => {
+  return (
+    <table>
+      <thead>
+      <tr>
+        <td>Quý khách có thể chọn lựa một trong cái dịch vụ sau</td>
+        <td>Cức dự tính</td>
+        <td>Thời gian phát</td>
+      </tr>
+      </thead>
+
+      <tbody>
+      { list.map( (item, index) =>
+        <tr>
+          <td>item[0]</td>
+          <td>item[1]</td>
+          <td>item[2]</td>
+        </tr>
+      )}
+      </tbody>
+    </table>
+  )
+};
+
+export default TablePrice;
