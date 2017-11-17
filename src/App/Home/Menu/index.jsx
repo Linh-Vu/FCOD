bea97e75425b7d2e690f0c9fb547840cd2b61c3e
Nguyen Ba Trinh <nguyenbatrinh0211@gmail.com>
2017-11-17 21:16:37 +0700

    scroll

--- src/App/Home/Menu/index.jsx
+++ src/App/Home/Menu/index.jsx
@@ -3,11 +3,71 @@ import React from 'react';
 import View from './View';
 
 class Menu extends React.Component {
-	render() {
-		return (
-			<View />
-		)
-	}
+  constructor(props) {
+    super(props);
+    this.scrollEvent = null;
+    this.node = null;
+    this.setNode = this.setNode.bind(this);
+    this.goto = this.goto.bind(this);
+  }
+
+  goto(value) {
+    // let current = Math.floor(window.scrollY);
+    // this.scrollEvent = setInterval(() => {
+    //   console.log('value:' + value);
+    //   if (current < value) {
+    //     current = current + 1;
+    //   }
+    //   if (current > value) {
+    //     current = current - 1;
+    //   }
+    //   window.scrollTo(0, current);
+    //   if (value === current) {
+    //     console.log('clear true');
+    //     clearInterval(this.scrollEvent);
+    //   }
+    this.scrollEvent(value);
+
+  }
+
+  scrollEvent (value) {
+    let current = Math.floor(window.scrollY);
+
+    if (current < value) {
+      current = current++;
+    }
+    if (current > value) {
+      current = current--;
+    }
+    window.scrollTo(0, current);
+    if ( current !== value ) {
+      setTimeout(this.scrollEvent(value), 1);
+    }
+    if ( current === value ) {
+      return;
+    }
+  }
+
+  setNode(node) {
+    this.node = node;
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY >= 42) {
+        this.node.style.position = 'fixed';
+        this.node.style.top = 0;
+      } else {
+        this.node.style.position = 'inherit';
+      }
+    });
+  }
+
+  render() {
+    return (
+      <View setNode={this.setNode} goto={this.goto}/>
+    )
+  }
 }
 
 export default Menu;
