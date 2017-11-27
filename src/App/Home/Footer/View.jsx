0a057e2ae77f20d794a89b8f90b831440264067c
nguyen trinh <nguyenbatrinh0211@gmail.com>
2017-11-27 21:26:43 +0700

    Footer (#9)
    * changed css
    
    * update css

--- src/App/Home/Footer/View.jsx
+++ src/App/Home/Footer/View.jsx
@@ -2,27 +2,33 @@ import React from 'react';
 
 import './styles.css';
 import logo from './image/logo.png';
+import phone from '../TopBar/icon/phone-call.svg';
+import location from '../TopBar/icon/location-pin.svg';
+import email from '../TopBar/icon/email.svg';
+
 
 const FooterView = () => {
-	return (
-		<div className="footer">
+  return (
+    <div className="footer">
       <div className="footer-content">
-        <div>
-          <img className="footer-logo" src={logo} alt="logo" />
-        </div>
+        <img className="footer-logo" src={logo} alt="logo"/>
 
-        <h2>Free Cash On Delivery</h2>
-        <hr />
-        
+
         <div className="footer-info">
-          <h4>19 Tự Cường - P.4 - Q. Tân Bình - TP. Hồ Chí Minh</h4>
-          <p>linhvu.vv@gmail.com</p>
-          <p>0909090909</p>
+          <h2>Free Cash On Delivery</h2>
+          <hr/>
+          <p><img src={location} alt="location"/>19 Tự Cường - P.4 - Q. Tân Bình - TP. Hồ Chí Minh</p>
+          <p><img src={email} alt="email"/>linhvu.vv@gmail.com</p>
+          <p><img src={phone} alt="phone"/>0909090909</p>
+          <div className="footer-info-support">
+            <textarea placeholder="..." rows="3" />
+            <button type="submit">Liên hệ</button>
+          </div>
         </div>
       </div>
       <p className="sub">Copyrights 2017 © fcod@gmail.com</p>
-		</div>
-	)
+    </div>
+  )
 };
 
 export default FooterView;
