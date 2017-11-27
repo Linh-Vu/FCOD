8c22e4358a47b929a3f7fa04b61b89cc38f40875
Nguyen Ba Trinh <nguyenbatrinh0211@gmail.com>
2017-11-27 21:09:49 +0700

    update css

--- src/App/Home/Footer/View.jsx
+++ src/App/Home/Footer/View.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 
 import './styles.css';
 import logo from './image/logo.png';
+import phone from '../TopBar/icon/phone-call.svg';
+import location from '../TopBar/icon/location-pin.svg';
+import email from '../TopBar/icon/email.svg';
+
 
 const FooterView = () => {
   return (
@@ -13,9 +17,13 @@ const FooterView = () => {
         <div className="footer-info">
           <h2>Free Cash On Delivery</h2>
           <hr/>
-          <h4>19 Tự Cường - P.4 - Q. Tân Bình - TP. Hồ Chí Minh</h4>
-          <p>linhvu.vv@gmail.com</p>
-          <p>0909090909</p>
+          <p><img src={location} alt="location"/>19 Tự Cường - P.4 - Q. Tân Bình - TP. Hồ Chí Minh</p>
+          <p><img src={email} alt="email"/>linhvu.vv@gmail.com</p>
+          <p><img src={phone} alt="phone"/>0909090909</p>
+          <div className="footer-info-support">
+            <textarea placeholder="..." rows="3" />
+            <button type="submit">Liên hệ</button>
+          </div>
         </div>
       </div>
       <p className="sub">Copyrights 2017 © fcod@gmail.com</p>
